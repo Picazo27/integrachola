d00a060fe08fb22f0ee88121dd68f88747695b47
Picazo27 <greciapicazoxd@gmail.com>
2023-12-04 16:52:52 -0600

    cambios

--- prototipo/src/views/Cliente/footer.vue
+++ prototipo/src/views/Cliente/footer.vue
@@ -1,43 +1,74 @@
 <template>
-    
-    <v-footer color="grey-darken-4">
-      
-        <v-row>
-        
-            <img  class="logop" src="../img/logopicazo.png" alt="Logo Empresa" height="50px">  <p>
-             <h1> Somos un gimnasio comprometido con tu bienestar. Ofrecemos una amplia gama de suplementos para ayudarte a alcanzar tus metas fitness.</h1>
-            </p>
-          
-          <v-col cols="8" md="5">
-            <h2>Contactanos por Redes Sociales:</h2>
-            <v-row>
-              <v-col cols="3">
-                <v-icon>mdi-facebook</v-icon>
-              </v-col>
-              <v-col cols="3">
-                <v-icon>mdi-whatsapp</v-icon>
-              </v-col>
-              <v-col cols="3">
-                <v-icon>mdi-instagram</v-icon>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-         
-    
-    </v-footer>
-  </template>
-  <style>
-.logop{
+  <v-footer color="grey-darken-4">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4" class="text-center">
+          <img src="../img/logopicazo.png" alt="Picazo Gym Logo" class="logop">
+          <p class="mt-4">Contamos con los mejores suplementos deportivos en línea. Enviamos a todo Torreón, Gómez Lerdo. Calidad 100% garantizada.</p>
+        </v-col>
+        <v-col cols="12" md="4">
+          <h3>SUPLEMENTOS DEPORTIVOS</h3>
+          <ul>
+            <li>Proteínas</li>
+            <li>Creatinas</li>
+            <li>Óxido Nítrico</li>
+            <li>Aminoácidos</li>
+            <li>BCAA</li>
+            <li>Quemadores de Grasa</li>
+            <li>Ganadores de Masa</li>
+          </ul>
+        </v-col>
+        <v-col cols="12" md="4">
+          <h3>SÍGUENOS</h3>
+          <ul>
+            <li>Facebook</li>
+            <li>Whatsapp</li>
+            <li>Instagram</li>
+          </ul>
+          <h3>CONTÁCTANOS</h3>
+          <ul>
+            <li>Teléfono: 123-456-789</li>
+            <li>Email: info@picazogym.com</li>
+          </ul>
+        </v-col>
+      </v-row>
+      <v-row align="center" justify="center">
+        <v-col>
+          <p class="mt-4">Contamos con los mejores suplementos deportivos en línea. Enviamos a todo Torreón, Gómez Lerdo. Calidad 100% garantizada.</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<style>
+  .logop {
     width: 110px;
-    height: 160px;
-    margin: 0%;
-}
-*{
-  font-family: Helvetica;
-}
-.contenedor{
-  position: static;
-}
+    height: auto;
+  }
+
+  ul {
+    list-style: none;
+    padding-left: 0;
+  }
+
+  li {
+    font-size: 18px;
+    margin-bottom: 8px;
+  }
+
+  p {
+    font-size: 16px;
+  }
+
+  @media screen and (max-width: 960px) {
+    .logop {
+      width: 80px;
+    }
+
+    li,
+    p {
+      font-size: 14px;
+    }
+  }
 </style>
-  
--- prototipo/src/views/Cliente/productos.vue
+++ prototipo/src/views/Cliente/productos.vue
@@ -40,107 +40,110 @@
     <hr>
   <br>
   <br>
-    <div class="mb-6">
-      <v-row>
-        <v-col cols="4">
-          <v-card-item>
-            <v-img src="~@/assets/logoofer.png" height="200"></v-img>
-            <v-card-title primary-title >
-              <div>
-                <h3>LOS MEJORES PRECIOS</h3>
-                <p>Te ofrecemos los precios más competitivos online</p>
-               
-              </div>
-            </v-card-title>
-            <v-card-text>
-            </v-card-text>
-          </v-card-item>
-        </v-col>
-        <v-col cols="4">
-          <v-card>
-            <v-img src="~@/assets/logoc.png" height="200"></v-img>
-            <v-card-title primary-title>
-              <div>
-                <h3>COMPRA EN PICAZO GYM</h3>
-                <p>Y recibe una gran experiencia de compra</p>
+  <v-row align="center" justify="center">
+  <v-col cols="12" md="4">
+    <v-card class="mx-auto" max-width="500">
+      <v-card-item>
+        <v-img src="~@/assets/logoofer.png" height="200" class="mb-5"></v-img>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="ce" >LOS MEJORES PRECIOS</h3>
+            <p class="ce">Te ofrecemos los precios más competitivos online</p>
+          </div>
+        </v-card-title>
+        <v-card-text></v-card-text>
+      </v-card-item>
+    </v-card>
+  </v-col>
 
-              </div>
-            </v-card-title>
-            <v-card-text>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col cols="4">
-          <v-card>
-            <v-img src="~@/assets/logoen.png" height="200"></v-img>
-            <v-card-title primary-title>
-              <div>
-                <h3>ENVÍOS MÁS RÁPIDOS</h3>
-                <p>Recibe tus suplementos de forma rápida y segura</p>
-              </div>
-            </v-card-title>
-            <v-card-text>
-       </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+  <v-col cols="12" md="4">
+    <v-card class="mx-auto" max-width="500">
+      <v-card-item>
+        <v-img src="~@/assets/logoc.png" height="200" class="mb-5"></v-img>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="ce">COMPRA EN PICAZO GYM</h3>
+            <p class="ce">Y recibe una gran experiencia de compra</p>
+          </div>
+        </v-card-title>
+        <v-card-text></v-card-text>
+      </v-card-item>
+    </v-card>
+  </v-col>
+
+  <v-col cols="12" md="4">
+    <v-card class="mx-auto" max-width="500">
+      <v-card-item>
+        <v-img src="~@/assets/logoen.png" height="200" class="mb-5"></v-img>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="ce">ENVÍOS MÁS RÁPIDOS</h3>
+            <p class="ce">Recibe tus suplementos de forma rápida y segura</p>
+          </div>
+        </v-card-title>
+        <v-card-text></v-card-text>
+      </v-card-item>
+    </v-card>
+  </v-col>
+</v-row>
+
       <br>
       
       <h1 class="red">Suplementos Para Gym</h1>
       <h2 class="ce">Contamos con una amplia variedad de suplementos deportivos para mejorar tu rendimiento físico tanto en el gym como en tu vida diaria. Ya sea que tu objetivo sea: mejorar tu rendimiento físico, aumentar tu masa muscular o seguir una dieta saludable para bajar de peso, aquí encontrarás los suplementos y vitaminas de la mas alta calidad en el mercado.</h2>    
-      </div>
+      
       <hr>
       <br>
-      <v-row>
-  <v-col cols="8" >
+      <v-row justify="center">
+  <v-col cols="8">
     <h1 class="red">Proteinas</h1>
-    <v-row>
-      <v-col cols="6">
+    <v-row align="center">
+      <v-col cols="12" sm="6">
         <h2>El consumo de proteína ayuda a los atletas a mejorar su rendimiento físico durante los entrenamientos o sesiones de ejercicio intenso. En nuestra tienda podrás encontrar una amplia variedad de suplementos de proteínas, desde proteína de suero hasta proteína de soja. Visíta nuestra Categoría de Proteínas</h2>
       </v-col>
-      <v-col cols="6">
+      <v-col cols="12" sm="6">
         <v-img src="https://whey.cl/wp-content/uploads/2020/11/cual-es-la-mejor-proteina-para-ganar-masa-muscular-sin-engordar-1.png"></v-img>
       </v-col>
     </v-row>
   </v-col>
   </v-row>
 
-  <v-row>
+  <v-row justify="center">
     <v-col cols="8">
 <h1 class="red">Creatinas</h1>
-<v-row>
-  <v-col cols="6">
+<v-row align="center"> 
+  <v-col cols="12" sm="6">
     <h2>Los suplementos de Monohidrato de Creatina también ayudan a incrementar tu rendimiento deportivo, especialmente si practicas deportes de fuerza. Además un beneficio adicional de la suplementación con creatina es que te ayudará a mejorar la ganancia de masa corporal sin causar efectos secundarios adversos.</h2>
   </v-col>
-  <v-col cols="6">
+  <v-col cols="12" sm="6">
     <v-img src="https://www.herbalife.com.pe/content/dam/regional/samcam/es_pe/sites/herbalife_nutrition/web_graphic/editorial/2022/05-may/Cuanta_proteina_debes_consumir_y_con_que_frecuencia.jpg"></v-img>
   </v-col>
 </v-row>
     </v-col>
   </v-row>
 
-<v-row>
+<v-row justify="center">
 <v-col cols="8">
   <h1 class="red">Aminoacidos</h1>
-<v-row>
-  <v-col cols="6">
+<v-row align="center">
+  <v-col cols="12" sm="6">
     <h2>La ingesta de suplementos de aminoácidos ayudan a la recuperación de los músculos después de las sesiones de ejercicio físico o de un entrenamiento de fuerza. La suplementación deportiva con éste tipo de productos también incrementa el rendimiento físico dado que el perfil de aminoácidos potencia el incremento muscular. </h2>
   </v-col>
-  <v-col cols="6">
+  <v-col cols="12" sm="6">
     <v-img src="https://mui.fitness/__export/1675972656166/sites/muifitness/img/2023/02/09/young-man-preparing-his-protein-drink-royalty-free-image-618752694-1546431880.jpg_1223424624.jpg"></v-img>
   </v-col>
 </v-row>
 </v-col>
 </v-row>
 
-<v-row>
+<v-row justify="center">
   <v-col cols="8">
     <h1 class="red">Ganadores de Peso</h1>
-<v-row>
-  <v-col cols="6">
+<v-row align="center">
+  <v-col cols="12" sm="6">
     <h2>Los ganadores de pesos son suplementos específicamente diseñados para proporcionar a la persona los macronutrientes, minerales, carbohidratos y la concentrarción de proteína necesaria para fomentar la ganancia de peso. Además un suplemento ganador de peso puede ayudar a contrarestar la pérdida de masa muscular generada por ejercicios cardiovasculares.</h2>
   </v-col>
-  <v-col cols="6">
+  <v-col cols="12" sm="6">
     <v-img src="https://whey.cl/wp-content/uploads/2020/11/cual-es-la-mejor-proteina-para-ganar-masa-muscular-sin-engordar-1.png"></v-img>
   </v-col>
 </v-row>
@@ -151,20 +154,25 @@
 </template>
 
 <style>
- img{
-  width: 750px;
-  height: 300px;
- }
- .red{
-  color:#B71C1C;
-  text-align: center;
- }
+@media screen and (min-width: 790px) {
+  .red {
+    color: #B71C1C;
+  }
+  img {
+    width: 750px;
+    height: 300px;
+  }
+}
+
+.max-width-sm{
+  max-width: 300px;
+}
+ 
+ 
   h1 {
     text-align: center;
   }
-  .ce{
-    text-align: center;
-  }
+  
 
   input {
     height: 30px;
@@ -196,21 +204,5 @@
       image: 'https://i0.wp.com/www.fitstore.com.mx/wp-content/uploads/2022/01/2264us_mutant_bcaa_9.7_tropical_mango_flavor_363_g_12.8_oz_v1.00-ms-1.png?fit=500%2C600&ssl=1'
     },
   ];
-  const promociones = [
-    {
-      variant:'outlined',
-      title:'¿PORQUÉ COMPRAR EN PICAZO GYM?',
-      image:'~@/assets/logoofer.png'
-    },
-    {
-      variant:'outlined',
-      title:'LOS MEJORES PRECIOS',
-      image:'~@/assets/logoc.png'
-    },
-    {
-      variant:'outlined',
-      title:'ENVÍOS MÁS RÁPIDOS',
-      image:'~@/assets/logoen.png'
-    }
-  ]
+  
 </script>
