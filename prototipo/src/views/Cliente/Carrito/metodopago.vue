010560e11e115a9cea395b6676a539db9ad961cb
Picazo27 <greciapicazoxd@gmail.com>
2023-12-05 10:39:46 -0600

    cambios

--- prototipo/src/views/Cliente/Carrito/metodopago.vue
+++ prototipo/src/views/Cliente/Carrito/metodopago.vue
@@ -1,24 +1,13 @@
 <template>
     <v-container>
       <v-card>
-        <v-card-title>Seleccionar Método de pago</v-card-title>
+        <v-card-title>Confirmar Entrega</v-card-title>
         <v-card-text>
-          <v-radio-group v-model="metodoDePago">
             <v-combobox
               label="Otras"
               :items="['Efectivo', 'Tarjeta']"
               variant="outlined"
             ></v-combobox>
-            <v-combobox
-            label="Tarjetas Guardadas"
-            :items="['Tarjeta1','Tarjeta2']"
-            variant="outlined"></v-combobox>
-          </v-radio-group>
-          <h3 class="pb-4 text-center">Agregar Nueva Tarjeta</h3>
-          <v-text-field  label="Número de Tarjeta"></v-text-field>
-          <v-text-field  label="Fecha de Vencimiento (MM/YY)"></v-text-field>
-          <v-text-field label="CVV"></v-text-field>
-          <v-checkbox label="Guardar Tarjeta"></v-checkbox>
         </v-card-text>
         <v-card-actions>
           <router-link to=""></router-link><v-btn color="error">Regresar</v-btn>
