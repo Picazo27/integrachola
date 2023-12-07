1772f60c56859f789b596808afb7f0a232c8a21f
Picazo27 <greciapicazoxd@gmail.com>
2023-12-07 14:46:33 -0600

    cambios

---
+++ prueba/src/views/inventariO.vue
@@ -0,0 +1,23 @@
+<template>
+
+<v-app>
+    <v-container>
+       <v-row><h1>Inventario</h1></v-row>
+hkhkhil
+       <v-row></v-row>
+    </v-container>
+</v-app>
+
+</template>
+
+
+<script>
+
+
+</script>
+
+<style>
+
+
+
+</style>
