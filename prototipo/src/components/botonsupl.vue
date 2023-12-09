82159b613c46383d73b9ba82a48c04027c8636f2
Picazo27 <greciapicazoxd@gmail.com>
2023-12-09 14:51:46 -0600

    cambios generales

--- prototipo/src/components/botonsupl.vue
+++ prototipo/src/components/botonsupl.vue
@@ -1,10 +1,32 @@
 <template>
-<v-btn 
-type="block"
-size="x-large"
-color="#D70000"
-class="mx-auto mt-6"
-style="width: 300px; height: 60px;" 
-@click="verSuplementos"
-></template>
-<script></script>
+    <v-btn 
+      type="block"
+      size="x-large"
+      color="#D70000"
+      class="mx-auto mt-6 custom-btn"
+      style="width: 300px; height: 60px;"
+      @click="verSuplementos"
+    ></v-btn>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      verSuplementos() {
+        // Tu lógica cuando se hace clic en el botón
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  .custom-btn {
+    transition: background-color 0.3s ease-in-out; /* Transición suave */
+  }
+  
+  /* Cambiar el color cuando se pasa el mouse */
+  .custom-btn:hover {
+    background-color: black;
+  }
+  </style>
+  
--- prototipo/src/components/section.vue
+++ prototipo/src/components/section.vue
@@ -4,15 +4,23 @@
         <v-col cols="6" class="d-flex flex-column align-center justify-center text-center">
     <h1 class="red xl">COMPRA AHORA</h1>
     <h3 class="se">Enviamos a todo México a través de nuestros socios en Amazon México. Pagos y Envíos 100% seguros. Suplementos Deportivos líderes en la industria y fabricados bajo estrictos controles de calidad.</h3>
-    
-   
+   <RouterLink to="suplementos"> <v-btn 
+      type="block"
+      size="x-large"
+      color="#D70000"
+      class="mx-auto mt-6 custom-btn"
+      style="width: 300px; height: 60px;"
+      @click="verSuplementos"
+    > Ver Suplementos</v-btn></RouterLink>
   </v-col>
         <v-col cols="6">
           <v-img src="https://blacknorange-mx.com/sdf/img/suplementos%202.png" aspect-ratio="1.6"></v-img>
         </v-col>
+        
       </v-row>
     </v-container>
   </template>
+
   <style>
 .sec{
     background-color: #212121;
@@ -22,6 +30,8 @@
     color: aliceblue;
 }
 </style>
+
   <script>
+  import { RouterLink } from 'vue-router';
   </script>
   
