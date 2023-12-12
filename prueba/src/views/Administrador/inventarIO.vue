7256cba236f24b4631fe725d57a215a36393fb58
Yoshua <df5280135@gmail.com>
2023-12-11 18:00:02 -0600

    arreglos generales

--- prueba/src/views/Administrador/inventarIO.vue
+++ prueba/src/views/Administrador/inventarIO.vue
@@ -1,55 +1,263 @@
-<template>
-<v-app>
-    <v-conatiner>
-        <h1 style="margin-top: 10px; margin-bottom: 18px; margin-left: 40px;">Proveedores</h1>
+<script>
+export default {
+  data: () => ({
+    dialog: false,
+    selectedProduct: null,
+    search: '',
+    headers: [
+      
+      { title:'Nombre',text: 'Nombre', value: 'nombre' },
+      { title: 'Categoria', text: 'Categoría', value: 'categoria' },
+      { title: 'Precio Compra', text: 'Precio Compra', value: 'precio_compra' },
+      { title: 'Precio Venta', text: 'Precio Venta', value: 'precio_venta' },
+      { title: 'Cantidad', text: 'Cantidad', value: 'cantidad' },
+      { title: 'Fecha Ingreso', text: 'Fecha Ingreso', value: 'fecha_ingreso' },
+      { title: 'Proveedor', text: 'Proveedor', value: 'proveedor' },
+      { title: 'Estatus', text: 'estado', value: 'estado' },
+    
+  ],
+    usuarios: [],
+    productosHeaders: [
+      { text: 'ID', value: 'id' },
+      { text: 'Nombre', value: 'nombre' },
+      { text: 'Cantidad', value: 'cantidad' },
+    ],
+    productos: [],
+    editedIndex: -1,
+    editedItem: {
+      id: '',
+      nombre: '',
+      estado: '',
+    },
+    defaultItem: {
+      id: '',
+      nombre: '',
+      estado: '',
+    },
+  }),
 
-        <v-row>
-         <v-col style="justify-content: center; align-content: center; margin-left: 70px;">
-            <v-text-field clearable label="Buscar" style="width:200px; "></v-text-field>
-         </v-col>
-         <v-col>
-            
-    <v-btn  size="30px">
-    <v-icon size="28px">mdi-plus</v-icon>
-</v-btn> 
-    <lalbel style="margin-left: 20px;">Añadir Producto</lalbel>
-         </v-col>
-         
-        </v-row>
-        <v-table fixed-header density="compact" style="margin: 200px; margin-top: 50px; width: 100%;">
-       <thead>
-         <tr>
-           
-           <th class="text-left">
-             Nombre 
-           </th>
-           
-         </tr>
-       </thead>
-       <tbody>
-         <tr
-           v-for="item in desserts"
-           :key="item.name"
-           
-         >
-           
-           <td>{{ item.Nombre }}
-            <v-btn style="margin-left: 480px; color: white; background-color: black;">Ver detalles</v-btn>
-            <v-icon style="margin-left: 25px;">mdi-delete</v-icon>
-        </td>
-           
-         
-         </tr>
-       </tbody>
-     </v-table>
-        <v-row>
-            
-        </v-row>
-    </v-conatiner>
-</v-app>
-</template>
+  computed: {
+    formTitle () {
+      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+    },
+  },
 
-<script setup>
+  watch: {
+    dialog (val) {
+      val || this.close()
+    },
+    dialogDelete (val) {
+      val || this.closeDelete()
+    },
+  },
 
+  created () {
+    this.initialize()
+  },
 
+  methods: {
+    initialize () {
+      this.usuarios = [
+      { id: 5724, nombre: 'Juan Perez', estado: 'Activo' },
+          { id: 5725, nombre: 'Grecia Pic', estado: 'Inactivo' },
+          { id: 5726, nombre: 'Angel Uwu', estado: 'Activo' },
+          { id: 5727, nombre: 'Juan Perez', estado: 'Activo' },
+          { id: 5728, nombre: 'Vic Vic', estado: 'Inactivo' },
+      ]
+    },
+
+    showProducts(item) {
+      this.productos = [
+        { id: 1, nombre: 'Producto A', cantidad: 2 },
+        { id: 2, nombre: 'Producto B', cantidad: 1 },
+      ];
+      this.dialog = true;
+    },
+
+    showProductDetails(productos) {
+      console.log("showProductDetails", productos);
+    },
+
+    closeProductDetails() {
+      // Cierra la sección de detalles del producto
+      this.selectedProduct = null;
+    },
+
+    editItem (item) {
+      this.editedIndex = this.usuarios.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    close () {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    closeDelete () {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+    save () {
+      if (this.editedIndex > -1) {
+        Object.assign(this.usuarios[this.editedIndex], this.editedItem)
+      } else {
+        this.usuarios.push(this.editedItem)
+      }
+      this.close()
+    },
+  },
+}
 </script>
+  
+  <template>
+    <v-data-table
+    :headers="headers"
+          :items="usuarios"
+          :search="search"
+          :sort-by="[{ key: 'calories', order: 'asc' }]"
+          style="width: 90%; margin-left: 60px;"
+    >
+      <template v-slot:top>
+        <v-toolbar
+        style="background-color: black;" flat
+        >
+          <v-toolbar-title style="color: aliceblue;"></v-toolbar-title>
+          <v-divider
+            class="mx-4"
+            inset
+            vertical
+          ></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog
+            v-model="dialog"
+            max-width="500px"
+          >
+            <template v-slot:activator="{ props }">
+              <v-text-field style="color: aliceblue;" v-model="search" append-icon="mdi-magnify" label="Buscar" single-line hide-details></v-text-field>
+             
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">Editar Estatus</span>
+              </v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.id"
+                        label="Orden"
+                        readonly
+                      ></v-text-field>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.nombre"
+                        label="Nombre"
+                        readonly
+                      ></v-text-field>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.estado"
+                        label="Estatus"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+  
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="grey-darken-4"
+                  variant="text"
+                  type="block"
+                  size="large"
+                  @click="close"
+                >
+                  Cancelar
+                </v-btn>
+                <v-btn
+                  color="grey-darken-4"
+                  variant="text"
+                  type="block"
+                  size="large"
+                  @click="save"
+                >
+                  Guardar
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+            <v-card v-if="selectedProduct">
+            <v-card-title>
+              Detalles del Producto
+            </v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <strong>ID:</strong> {{ selectedProduct.id }}
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <strong>Nombre:</strong> {{ selectedProduct.nombre }}
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <strong>Cantidad:</strong> {{ selectedProduct.cantidad }}
+                </v-col>
+              </v-row>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="grey-darken-4" type="block" siz="large" @click="closeProductDetails">Cerrar</v-btn>
+            </v-card-actions>
+         # </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon
+          size="small"
+          class="me-2"
+          @click="editItem(item)"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+          size="small"
+          class="me-2"
+          @click="showProductDetails(item)"
+        >
+        mdi-vector-point
+        </v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-btn
+          color="grey-darken-4"
+          size="large"
+          type="block"
+          @click="initialize"
+        >
+          Reset
+        </v-btn>
+      </template>
+    </v-data-table>
+  </template>
+  
