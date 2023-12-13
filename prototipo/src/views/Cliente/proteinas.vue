f9342aee0679ab4c787fe3272d6a54806019d518
Picazo27 <greciapicazoxd@gmail.com>
2023-12-12 19:24:46 -0600

    cambios pic

--- prototipo/src/views/Cliente/proteinas.vue
+++ prototipo/src/views/Cliente/proteinas.vue
@@ -1,41 +1,40 @@
 <template>
-    <v-container>
-        <h1>Proteinas</h1>
-        <br>
-      <v-row align="center" justify="center">
-        <v-col
-          v-for="(variant, i) in variants"
-          :key="i"
-          cols="auto"
+  <v-container>
+    <h1>Proteinas</h1>
+    <br>
+    <v-row align="center" justify="center">
+      <v-col
+        v-for="(protein, index) in proteins"
+        :key="index"
+        cols="auto"
+      >
+        <v-card
+          class="mx-auto"
+          max-width="344"
         >
-          <v-card
-            class="mx-auto"
-            max-width="344"
-            :variant="variant"
-          >
-            <v-card-item>
-              <div>
-                <div class="text-overline mb-1">
-                  {{ variant }}
-                </div>
-                <div class="text-h6 mb-1">
-                  Proteina
-                </div>
-                <div class="prote"><img src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1664439072-41Veol69hzL._SL500_.jpg?crop=1xw:1xh;center,top&resize=980:*"></div>
+          <v-card-item>
+            <div>
+              <div class="text-overline mb-1">
+                {{ protein.variant }}
               </div>
-            </v-card-item>
-  
-            <v-card-actions>
-              <v-btn>
-                Agregar al Carrito
-              </v-btn> 
-              <input type="number">
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
+              <div class="text-h6 mb-1">
+                {{ protein.nombre_producto }}
+              </div>
+              <div class="prote"><img :src="protein.imagen" alt="Proteina" style="height: 350px; width: 350px;"></div>
+            </div>
+          </v-card-item>
+
+          <v-card-actions>
+            <v-btn @click="addToCart(protein)">
+              Agregar al Carrito
+            </v-btn>
+            <input type="number">
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 
   <style>
 img{
@@ -53,5 +52,18 @@ input{
 
 </style>
 <script setup>
-  const variants = ['outlined', 'outlined', 'outlined','outlined']
+ const proteins = ref([]);
+
+  import axios from 'axios';
+
+  onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost/mostrar');
+    proteins.value = response.data.data; // Asegúrate de ajustar esto según la estructura de tus datos
+  } catch (error) {
+    console.error('Error fetching proteins:', error);
+    alert('Hubo un error al cargar los datos de las proteínas. Por favor, inténtalo de nuevo más tarde.');
+  }
+});
+
 </script>
